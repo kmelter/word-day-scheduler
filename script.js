a1afbb4bcb8138d903310df5e1fbb4172ced9683
kmelter <kmelter97@gmail.com>
2022-03-09 16:40:38 -0600

    initial local storage

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var tasks;
+var currentHour = (new Date()).getHours();
+
 // display current time
 var datetime = null,
         date = null;
@@ -15,13 +18,13 @@ $(document).ready(function(){
 });
 
 // color coding each hour
-var currentHour = (new Date()).getHours();
+
 $('.edit_cont')
   .each(function(){
     var val = parseInt($(this).prop('id'));
-    if(val > currentHour && val < currentHour + 12){
+    if(val > currentHour && val < currentHour + 24){
       $(this).css('background-color','Green');
-    }else if(val < currentHour && val > currentHour - 12){
+    }else if(val < currentHour && val > currentHour - 24){
       $(this).css('background-color','LightGrey');
     }else if(val === currentHour){
       $(this).css('background-color','Red');
@@ -33,14 +36,17 @@ $('.edit_cont')
 // TODO: when savebutton is clicked, submit content of task box to local storage
 $(".saveBtn").on("click", function() {
     tasks = JSON.parse(localStorage.getItem("tasks"));
-  
+    var taskList = document.getElementById("8").textContent
+
     // if nothing in localStorage, create a new object to track all task status arrays
     if (!tasks) {
         tasks = {
-        toDo: []
+        toDo: [taskList]
       };
     }
-  
+
+    localStorage.setItem("hour" + currentHour, tasks);
+
     // loop over object properties
     $.each(tasks, function(list, arr) {
       console.log(list, arr);
