67ff89a84520d0271c08a598a1c07279f3d916a9
kmelter <kmelter97@gmail.com>
2022-03-12 17:17:23 -0600

    save button functionality

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 var currentHour = (new Date()).getHours();
-var saveButton
 
 var tasks = {
-    "8": [],
-    "9": [],
-    "10": [],
-    "11": [],
-    "12": [],
-    "13": [],
-    "14": [],
-    "15": [],
-    "16": [],
-    "17": [],
-    "18": []
+  "8": [],
+  "9": [],
+  "10": [],
+  "11": [],
+  "12": [],
+  "13": [],
+  "14": [],
+  "15": [],
+  "16": [],
+  "17": [],
+  "18": []
+};
+
+//function to get localstorage object, parse it, then update array content
+var updateTasks = function() {
+  var taskList = JSON.parse(localStorage.getItem("tasks"));
+  taskList["8"].push("test");
+  localStorage.setItem("tasks", JSON.stringify(taskList));
 };
 
 var setTasks = function() {
@@ -41,35 +47,30 @@ $('.edit_cont')
   .each(function(){
     var val = parseInt($(this).prop('id'));
     if(val > currentHour && val < currentHour + 24){
-      $(this).css('background-color','Green');
+      $(this).css('background-color','LimeGreen');
     }else if(val < currentHour && val > currentHour - 24){
       $(this).css('background-color','LightGrey');
     }else if(val === currentHour){
-      $(this).css('background-color','Red');
+      $(this).css('background-color','Crimson');
     }else{
       $(this).css('background-color','White');
     }
 });
 
+
+
 // TODO: when savebutton is clicked, submit content of task box to local storage
 $(".saveBtn").on("click", function() {
   var taskList = JSON.parse(localStorage.getItem("tasks"));
-  if (!tasks) {
-    var tasks = {
-      "8": [],
-      "9": [],
-      "10": [],
-      "11": [],
-      "12": [],
-      "13": [],
-      "14": [],
-      "15": [],
-      "16": [],
-      "17": [],
-      "18": []
-  };
+  if (!taskList) {
     setTasks();
   }
   console.log(taskList);
   //if button id = "b" + textarea id then update localstorage...
+  if ($(this).attr("id") === "b" + document.getElementsByName("textarea").id) {
+    //get the save button id and slice off the "b". saveButtonId will then hold the resulting number
+    var saveButtonId = $(this).attr("id").slice(1); //"b15" -> "15"
+    //saveButtonId.parseInt();
+    updateTasks();
+  }
 });
